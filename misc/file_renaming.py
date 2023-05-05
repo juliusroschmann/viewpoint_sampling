8d567021a13b96b506ea2e0a0d1d9b5c24afefea
Julius Roschmann <mr18b029@protonmail.com>
2023-05-05 16:54:47 +0200

    Added training_automation.py and minor changes

--- misc/file_renaming.py
+++ misc/file_renaming.py
@@ -1,30 +1,37 @@
 
 
-pwd = "/home/administrator/Documents/recordings/Julius/scenes/002_lying_liquid_plus/rgb/"
+pwd = "/home/julius/Documents/Julius_03/scenes/004_lying_liquid_84+/depth/"
+old_file = pwd + "associations_cut.txt"
 
-# filename = "associations.txt"
-# pwd += filename
+new_file = pwd + "associations_new.txt"
+#pwd += filename
 
-#groundtruth
-# with open(pwd, "rt") as fin:
-#     with open(filename, "wt") as fout:
+index = 85
+
+##groundtruth
+# with open(old_file, "rt") as fin:
+#     with open(new_file, "wt") as fout:
 #         for idx, line in enumerate(fin):
 #             print(line)
-#             fout.write(line.replace(str(idx+1), str(idx+339), 1))
+#             fout.write(line.replace(str(idx+1), str(idx+index), 1))
 
-#associations
-# with open(pwd, "rt") as fin:
-#     with open(filename, "wt") as fout:
+##associations
+# with open(old_file, "rt") as fin:
+#     with open(new_file, "wt") as fout:
 #         for idx, line in enumerate(fin):
 #             print(line)
-#             line = line.replace(str(idx+1)+" ", str(idx+339)+" ")
-#             fout.write(line.replace(str(idx+1).zfill(6), str(idx+339).zfill(6)))
+#             line = line.replace(str(idx+1)+" ", str(idx+index)+" ")
+#             fout.write(line.replace(str(idx+1).zfill(6), str(idx+index).zfill(6)))
 
 
 #image renaming
 import os
+import shutil
 
-for i, filename in enumerate(sorted(os.listdir(pwd))):
-    print(filename)
-    os.rename(pwd + filename, pwd + str(i+339).zfill(6) + ".png")
+for i, file in enumerate(sorted(os.listdir(pwd))):
+    print(len(os.listdir(pwd)))
+    print("renaming: {} to: {}".format(pwd + file,  pwd + str(i+index).zfill(6) + ".png"))
+    #print("New: ", str(i+index).zfill(6) + ".png")
+    #os.rename(pwd + file, pwd + str(i+index).zfill(6) + ".png")
+    shutil.copy(pwd + file,  "/home/julius/Documents/Julius_03/scenes/test_004/depth/" + str(i+index).zfill(6) + ".png")
 
--- misc/training_automation.py
+++ misc/training_automation.py
@@ -1,65 +1,151 @@
 #!/usr/bin/env python3
 
-#import fps
-import glob
+import io
+import signal
 import subprocess
 import numpy as np
 import os
-import pathlib
+import time
 
-n_samples = np.arange(1, 424, 50, dtype=int)
+n_samples = np.arange(50, 424, 50, dtype=int)
 n_samples = np.append(n_samples, 424).tolist()
 n_samples = [str(sample) for sample in n_samples]
 print(n_samples)
 
-#subprocess.Popen(["python3", "fps.py"] + list(n_samples), capture_output=True)
-
-# for simple commands
-try:
-    success = subprocess.run(["ls", "-l", "/gki,"], check=True, capture_output=True) 
-    if success.returncode==0:
-        print("")
-        print("SUCCESS")
-    else:
-        print("")
-        print("FAILED")
-except Exception as e:
-    print("EXCEPTION: ", e) 
-
-
-# for complex commands, with many args, use string + `shell=True`:
-# cmd_str = "ls -l /tmp | awk '{print $3,$9}' | grep root"
-# subprocess.run(cmd_str, shell=True)
-
-
-methods = ["nerfacto", "instant-ngp-bounded"]
+methods = ["nerfacto", "instant-ngp","instant-ngp-bounded"]
+#methods = ["instant-ngp"]
 sigma = 15
-scenes = ["001_standing_liquid", "002_lying_liquid", "003_standing_empty", "005_lying_empty", "006_standing_coated", "007_lying_coated"]
 
-cmd_train = "ns-train " + methods[0] + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --data /home/julius/nerfstudio-tracebot/data/Julius/scenes/" + scenes[0] + " tracebot-data"
-cmd_eval = "ns-eval --load-config /home/julius/outputs/" + scenes[0] + "/" + methods[0] + "/2023-04-17_145315/config.yml" 
-print(cmd_eval)
 
-for method in methods:
-    for scene in scenes:
-        cmd_train = "ns-train " + method + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --data /home/julius/nerfstudio-tracebot/data/Julius/scenes/" + scene + " tracebot-data"
-        #cmd_eval = "ns-eval --load-config /home/julius/outputs/" + scene + "/" + method + "/"
-        cmd_eval = "~/Desktop"
-        #print(pathlib.Path(cmd_eval).glob('*/'))
-        print(sorted(glob.glob(os.path.join(cmd_eval, '*/')), key=os.path.getmtime))
-        
-        folder = max(pathlib.Path(cmd_eval).glob('*/'), key=os.path.getmtime)
-        cmd_eval = "ns-eval --load-config /home/julius/outputs/" + scene + "/" + method + "/" + folder + "/config.yml" 
+scenes = sorted([f.name for f in os.scandir("/home/julius/Julius_03_960x540/scenes") if f.is_dir()])
 
+print(scenes)
+
+#scenes = ["001_standing_coated_960x540", "002_lying_coated_960x540"]
+#scenes = ["002_lying_coated_960x540"]
+
+#cmd_train = "ns-train " + methods[0] + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --data /home/julius/Julius_03_960x540/scenes/" + scenes[0] + " tracebot-data"
+#cmd_eval = "ns-eval --load-config /home/julius/outputs/" + scenes[0] + "/" + methods[0] + "/_____INSERT_FOLDER_HERE______/config.yml" 
+#print(cmd_eval)
+
+eval_list = []
+
+for scene in scenes:
+    for method in methods:
+        if method == "nerfacto":
+            iterations = 5000
+        else:
+            iterations = 10000
+        cmd_train = "ns-train " + method + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --max-num-iterations " + str(iterations) + " --data /home/julius/Julius_03_960x540/scenes/" + scene + " tracebot-data"
         print(cmd_train)
-        print(pathlib.Path(cmd_eval).glob('*/'))
+        #cmd_eval = "ns-eval --load-config /home/julius/outputs/" + scene + "/" + method 
+        cmd_train = cmd_train.split()
+
+        try:
+            #subprocess.check_call(cmd_train, cwd=os.getcwd())
+            print("Trying to run: ", cmd_train)
+            p = subprocess.Popen(cmd_train, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            flag = False
+            #print("CHECK OUTPUT: ", subprocess.check_output(cmd_train, stderr=subprocess.STDOUT))
+            for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):  # or another encoding
+                #print(line)
+                if flag == True:
+                    # print("----------------------------------------------------------------")
+                    # print(line)
+                    # print("----------------------------------------------------------------")
+                    # print("")
+                    print(line)
+                    cmd_eval = "ns-eval --load-config /home/julius/" + str(line.replace(" ", ""))
+                    cmd_eval = cmd_eval.replace("\n", "")
+                    #cmd_eval = cmd_eval.split()
+                    eval_list.append(cmd_eval)
+                    flag = False
+                
+                if "Saving config to:" in line:
+                    flag = True
+                   
+                if "Training Finished" in line:
+                    time.sleep(2)
+                    p.send_signal(signal.SIGINT)
+            
+            p.wait()
+            # print("")
+            # print("----------------------------------SUCCESS")
+            # print("")
+            # p.send_signal(signal.SIGINT)
+        except subprocess.CalledProcessError:
+            print("There was an error with command ____cmd_train____ exited with non-zero code!")
+
         print("")
+        print("Now training next scene.")
+        print("")
+        p.kill()
+print("")
+print("")
+print("")
+print("----------Finished training----------")
+print("Eval List: ", eval_list)
 
-# "ns-train nerfacto --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh 15 --data /home/julius/nerfstudio-tracebot/data/Julius/scenes/003_standing_empty tracebot-data"
-# "ns-eval --load-config /home/julius/outputs/003_standing_empty/nerfacto/2023-04-17_145315/config.yml" 
+# p1 =  subprocess.Popen(eval_list[0], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# p2 =  subprocess.Popen(eval_list[1], stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# print("Output from process 2: " + (p2.communicate()[0]).decode())
+# print("")
+# for line in io.TextIOWrapper(p2.stdout, encoding="utf-8"):  # or another encoding
+#         print(line)
 
-# "ns-train nerfacto --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh 15 --data /home/julius/nerfstudio-tracebot/data/Julius/scenes/003_standing_empty tracebot-data --downscale_factor 2"
-# "ns-eval --load-config ~/outputs/003_standing_empty/nerfacto/2023-04-18_094805/config.yml"
+#procs = [ subprocess.Popen(i, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) for i in eval_list]
 
-# "ns-train nerfacto --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh 15 --data /home/julius/nerfstudio-tracebot/data/Julius/scenes/003_standing_empty tracebot-data --downscale_factor 4"
-# "ns-eval --load-config ~/outputs/003_standing_empty/nerfacto/2023-04-18_100338/config.yml"
+# for idx, p in enumerate(procs):
+    
+#     for line in io.TextIOWrapper(p2.stdout, encoding="utf-8"):  # or another encoding
+#         print(line)
+#         break
+    
+#     #print(s_out)
+#     print("trying to print s_out")
+#     p.wait()
+#     print("finished waiting")
+#     p.kill()
+
+
+    
+# for item in eval_list:
+#     item = item.split()
+#     print("Item: ", item)
+#     try:
+#         p = subprocess.Popen(item, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+#         p.wait()
+#         print("Waiting for subprocess!")
+#     except subprocess.CalledProcessError:
+#         print("There was an error with command ____cmd_eval____ exited with non-zero code!")
+
+
+# for scene in scenes:
+#     for method in methods:
+#         # path = "/home/julius/outputs/" + scene + "/" + method 
+#         # print("Path:", path)
+#         # print("Globbing: ", [f.path for f in os.scandir(path) if f.is_dir()])
+#         # cmd_eval = "ns-eval --load-config" + max([f.path for f in os.scandir(path) if f.is_dir() ], key=os.path.getmtime) + "/config.yml"
+#         # cmd_eval = cmd_eval.split()
+
+#         # try:
+#         #     #subprocess.check_call(cmd_eval, cwd=os.getcwd())
+#         #     print("Trying to run: ", cmd_eval)
+#         #     p = subprocess.Popen(cmd_eval, stdin=subprocess.PIPE, stdout=subprocess.PIPE) 
+#         #     print("")
+#         #     print("----------------------------------SUCCESS")
+#         #     print("")
+#         #     p.send_signal(signal.SIGINT)
+#         # except subprocess.CalledProcessError:
+#         #     print("There was an error with command ____cmd_eval____ exited with non-zero code!")
+       
+#         # p.kill()
+
+#         #/home/julius/Julius_03_960x540/scenes/001_standing_coated_960x540
+#         #scp -r julius@192.168.141.40:/home/julius/Julius_03_960x540/scenes/001_standing_coated_960x540/depth_nerf /home/julius/Documents/auswertung_03/001_standing_coated_960x540/instant-ngp/depth_nerf
+#         copy_cmd = "scp -r julius@192.168.141.40:/home/julius/Julius_03_960x540/scenes/" + scene + "/depth_nerf"
+#         copy_cmd = copy_cmd.split()
+
+
+#ns-train instant-ngp --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh 15 --data /home/julius/Julius_03_960x540/scenes/001_standing_coated_960x540 tracebot-data --viewer.websocket-port 7008
+#ns-train instant-ngp --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh 15 --data /home/julius/Julius_03_960x540/scenes/002_lying_coated_960x540 tracebot-data
