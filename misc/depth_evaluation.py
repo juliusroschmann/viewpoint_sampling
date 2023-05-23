1aeea2e1821d8200fd5dce9185dcb3a47ee2a470
Julius Roschmann <mr18b029@protonmail.com>
2023-05-23 12:55:37 +0200

    Depth eval working but problem with instant-dex

--- misc/depth_evaluation.py
+++ misc/depth_evaluation.py
@@ -1,167 +1,216 @@
 #!/usr/bin/env python3
 from skimage.io import imread, imread_collection
+import ipdb
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pandas as pd
 
-# Read the image as a float
-folder_per_obj_masks = '/home/julius/Documents/Julius_03_masks'
-folder_full_blender_depth = '/home/julius/Documents/Julius_03_depth_masks'
-scenes = sorted(os.listdir(folder_per_obj_masks))
 
-canister_list = []
-small_bottle_list = []
-medium_bottle_list = []
-large_bottle_list = []
-full_depth_list = []
-to_eval_imgs_list = []
+def main():
+    # Read the image as a float
+    folder_per_obj_masks = '/home/julius/Documents/Julius_03_masks'
+    folder_full_blender_depth = '/home/julius/Documents/Julius_03_depth_masks'
+    scenes = sorted(os.listdir(folder_per_obj_masks))
 
-##here implement for loop over all scenes
-for scene in [scenes[0]]:
-    folder_to_evaluate = f'/home/julius/Documents/Julius_03_auswertung/scenes_trained_with_other_params/{scene}/nerfacto/depth_nerf_sigma_10_iter_5000/depth_nerf'
+    obj_list = ["canister", "small_bottle", "medium_bottle", "large_bottle"]
+    title_list = []
+    for obj in obj_list:
+        title_list.append(f"{obj}_gt")
+        title_list.append(f"{obj}_real")
+        title_list.append(f"{obj}_diff")
 
-    folder1=os.path.join(folder_per_obj_masks, scene)
-    for img in sorted(os.listdir(folder1)):
-        image = (os.path.join(folder1, img))
-        if "canister" in img:
-            canister_list.append(image)
-        elif "small" in img:
-            small_bottle_list.append(image)
-        elif "medium" in img:
-            medium_bottle_list.append(image)
-        elif "large" in img:
-            large_bottle_list.append(image)
+    canister_list = []
+    small_bottle_list = []
+    medium_bottle_list = []
+    large_bottle_list = []
+    full_depth_list = []
+    to_eval_imgs_list = []
 
-    canister_masks = imread_collection(canister_list)
-    small_bottle_masks = imread_collection(small_bottle_list)
-    medium_bottle_masks = imread_collection(medium_bottle_list)
-    large_bottle_masks = imread_collection(large_bottle_list)
+    # here implement for loop over all scenes
+    for scene in [scenes[0]]:
+        # folder_to_evaluate = f'/home/julius/Documents/Julius_03_auswertung/scenes_trained_with_other_params/{scene}/nerfacto/depth_nerf_sigma_10_iter_5000/depth_nerf'
+        folder_to_evaluate = f"/home/julius/Documents/Julius_03_x_auswertung/Julius_03_20/scenes/{scene}/depth_nerf"
+        folder_to_save = os.path.dirname(os.path.dirname(os.path.dirname(folder_to_evaluate)))
+        folder_to_save += f"/results_nerfacto_{scene}.csv"
 
-    folder2=os.path.join(folder_full_blender_depth, scene)
-    for img in sorted(os.listdir(folder2)):
-        image = (os.path.join(folder2, img))
-        full_depth_list.append(image)
+        folder1 = os.path.join(folder_per_obj_masks, scene)
+        for img in sorted(os.listdir(folder1)):
+            image = (os.path.join(folder1, img))
+            if "canister" in img:
+                canister_list.append(image)
+            elif "small" in img:
+                small_bottle_list.append(image)
+            elif "medium" in img:
+                medium_bottle_list.append(image)
+            elif "large" in img:
+                large_bottle_list.append(image)
 
-    blender_depth = imread_collection(full_depth_list)
+        canister_masks = imread_collection(canister_list)
+        small_bottle_masks = imread_collection(small_bottle_list)
+        medium_bottle_masks = imread_collection(medium_bottle_list)
+        large_bottle_masks = imread_collection(large_bottle_list)
 
-    for img in sorted(os.listdir(folder_to_evaluate)):
-        image = (os.path.join(folder_to_evaluate, img))
-        to_eval_imgs_list.append(image)
+        folder2 = os.path.join(folder_full_blender_depth, scene)
+        for img in sorted(os.listdir(folder2)):
+            image = (os.path.join(folder2, img))
+            full_depth_list.append(image)
 
-    to_eval_imgs = imread_collection(to_eval_imgs_list)
+        blender_depth = imread_collection(full_depth_list)
 
-    if len(canister_masks) != len(blender_depth) or len(small_bottle_masks) != len(blender_depth) or len(medium_bottle_masks) != len(blender_depth) or len(large_bottle_masks) != len(blender_depth) or len(to_eval_imgs) != len(blender_depth):
-        print("canister_masks: ", len(canister_masks))
-        print("small_bottle_masks: ", len(small_bottle_masks))
-        print("medium_bottle_masks: ", len(canister_masks))
-        print("large_bottle_masks: ", len(large_bottle_masks))
-        print("blender_depth: ", len(blender_depth))
-        print("to_eval_imgs: ", len(to_eval_imgs))
-        
-        raise ValueError (f"Lists do not have the same size! Check folder {folder1} or {folder2} or {folder_to_evaluate}!")
+        for img in sorted(os.listdir(folder_to_evaluate)):
+            image = (os.path.join(folder_to_evaluate, img))
+            to_eval_imgs_list.append(image)
+
+        to_eval_imgs = imread_collection(to_eval_imgs_list)
+
+        if len(canister_masks) != len(blender_depth) or len(small_bottle_masks) != len(blender_depth) or len(medium_bottle_masks) != len(blender_depth) or len(large_bottle_masks) != len(blender_depth) or len(to_eval_imgs) != len(blender_depth):
+            print("canister_masks: ", len(canister_masks))
+            print("small_bottle_masks: ", len(small_bottle_masks))
+            print("medium_bottle_masks: ", len(canister_masks))
+            print("large_bottle_masks: ", len(large_bottle_masks))
+            print("blender_depth: ", len(blender_depth))
+            print("to_eval_imgs: ", len(to_eval_imgs))
+
+            raise ValueError(
+                f"Lists do not have the same size! Check folder {folder1} or {folder2} or {folder_to_evaluate}!")
+
+        else:
+            mydict = {"File": [], "Scene": [], "Object": [], "Max": [],
+                      "Min": [], "Median": [], "Mean": [], "Std": [], "Var": []}
+            for idx, _ in enumerate(blender_depth):
+                
+                filename = os.path.basename(full_depth_list[idx])
+
+                canister_gt = np.where(
+                    canister_masks[idx] > 0, blender_depth[idx], 0)
+                small_bottle_gt = np.where(
+                    small_bottle_masks[idx] > 0, blender_depth[idx], 0)
+                medium_bottle_gt = np.where(
+                    medium_bottle_masks[idx] > 0, blender_depth[idx], 0)
+                large_bottle_gt = np.where(
+                    large_bottle_masks[idx] > 0, blender_depth[idx], 0)
+                gt_list = [canister_gt, small_bottle_gt,
+                           medium_bottle_gt, large_bottle_gt]
+
+                canister_real = np.where(
+                    canister_masks[idx] > 0, to_eval_imgs[idx], 0)
+                small_bottle_real = np.where(
+                    small_bottle_masks[idx] > 0, to_eval_imgs[idx], 0)
+                medium_bottle_real = np.where(
+                    medium_bottle_masks[idx] > 0, to_eval_imgs[idx], 0)
+                large_bottle_real = np.where(
+                    large_bottle_masks[idx] > 0, to_eval_imgs[idx], 0)
+                real_list = [canister_real, small_bottle_real,
+                             medium_bottle_real, large_bottle_real]
+
+                # #canister_diff = canister_gt - canister_real
+                # canister_diff = np.random.randint(0,10, size=1280*720).reshape((720,1280))
+                # small_bottle_diff = small_bottle_gt - small_bottle_real
+                # medium_bottle_diff = medium_bottle_gt - medium_bottle_real
+                # large_bottle_diff = large_bottle_gt - large_bottle_real
+                # full_diff = blender_depth[idx] - to_eval_imgs[idx]
+
+
+                # ACHTUNG CANISTER HAT LOCH --> #PIXEL ANDERS
+                # canister_diff = np.random.randint(
+                #     0, 10, size=1280*720).reshape((720, 1280))
+                canister_diff = np.nan
+                small_bottle_diff = calc_diff(
+                    True, small_bottle_gt, small_bottle_real)
+                medium_bottle_diff = calc_diff(
+                    True, medium_bottle_gt, medium_bottle_real)
+                large_bottle_diff = calc_diff(
+                    True, large_bottle_gt, large_bottle_real)
+
+                diff_list = [canister_diff, small_bottle_diff,
+                             medium_bottle_diff, large_bottle_diff]
+
+                master_list = [canister_gt, canister_real, canister_diff, small_bottle_gt, small_bottle_real, small_bottle_diff,
+                               medium_bottle_gt, medium_bottle_real, medium_bottle_diff, large_bottle_gt, large_bottle_real, large_bottle_diff]
+                # canister_vals = calc_diff(canister_gt, canister_real)
+                small_bottle_vals = calc_diff(
+                    False, small_bottle_gt, small_bottle_real)
+                medium_bottle_vals = calc_diff(
+                    False, medium_bottle_gt, medium_bottle_real)
+                large_bottle_vals = calc_diff(
+                    False, large_bottle_gt, large_bottle_real)
+                
+                # print("small_bottle_vals: ", small_bottle_vals)
+                # print("medium_bottle_vals: ", medium_bottle_vals)
+                # print("large_bottle_vals: ", large_bottle_vals)
+                # print()
+
+                # mydict = append_to_dict(mydict, filename, scene, canister_vals, object_name=obj_list[0])
+                mydict = append_to_dict(
+                    mydict, filename, scene, small_bottle_vals, object_name=obj_list[1])
+                mydict = append_to_dict(
+                    mydict, filename, scene, medium_bottle_vals, object_name=obj_list[2])
+                mydict = append_to_dict(
+                    mydict, filename, scene, large_bottle_vals, object_name=obj_list[3])
+
+                # fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(20, 15))
+                # plt.set_cmap('plasma')
+                # plt.suptitle(f"Image {idx}")
+                # for idx, ax in enumerate(axes.flat):
+                #     ax.set_axis_off()
+                #     im = ax.imshow(master_list[idx])
+                #     ax.set_title(title_list[idx])
+
+                # cb_ax = fig.add_axes([0.9, 0.1, 0.02, 0.8])
+                # cbar = fig.colorbar(im, cax=cb_ax)
+                # cbar.set_ticks(np.arange(0, 100.5, 1))
+                # # cbar.set_ticklabels(['low', 'medium', 'high'])
+                # plt.show()
+   
+    df = pd.DataFrame.from_dict(data=mydict).to_csv(folder_to_save)
+    print(f"Saving dataframe to: {folder_to_save}")
+    #print(df.head(100))
+    print(df["Max"].mean(skipna=True))
+    print(df["Min"].mean(skipna=True))
+    print(df["Median"].mean(skipna=True))
+    print(df["Mean"].mean(skipna=True))
+
+
+def calc_diff(just_diff, obj_gt, obj_real):
+
+    new_img = np.zeros_like(obj_gt)
+    x = np.argwhere(obj_gt)[:, 0]
+    y = np.argwhere(obj_gt)[:, 1]
     
+    px_gt = obj_gt[x, y]
+    px_real = obj_real[x, y]
+    px_diff = np.abs(px_gt - px_real)
+    #print(px_diff)
+    px_diff_clipped = np.clip(np.abs(px_diff), 0, 100)
+    #print(px_diff)
+    new_img[x, y] = px_diff_clipped
+
+    if just_diff:
+        # print(f"px_gt median: {np.median(px_gt):.4f}, px_real median: {np.median(px_real):.4f}, diff factor: {(np.median(px_gt) / np.median(px_real)):.4f}" )
+        # print(f"px_gt mean: {np.mean(px_gt):.4f}, px_real mean: {np.mean(px_real):.4f}, diff factor: {(np.mean(px_gt) / np.mean(px_real)):.4f}" )
+        # print()
+        return new_img
+
+    if len(px_diff) == 0:
+        return [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
     else:
-        for idx, item in enumerate(blender_depth):
-           
-            canister_gt = np.where(canister_masks[idx] > 0, blender_depth[idx], 0)
-            small_bottle_gt = np.where(small_bottle_masks[idx] > 0, blender_depth[idx], 0)
-            medium_bottle_gt = np.where(medium_bottle_masks[idx] > 0, blender_depth[idx], 0)
-            large_bottle_gt = np.where(large_bottle_masks[idx] > 0, blender_depth[idx], 0)
-
-            canister_real = np.where(canister_masks[idx] > 0, to_eval_imgs[idx], 0)
-            small_bottle_real = np.where(small_bottle_masks[idx] > 0, to_eval_imgs[idx], 0)
-            medium_bottle_real = np.where(medium_bottle_masks[idx] > 0, to_eval_imgs[idx], 0)
-            large_bottle_real = np.where(large_bottle_masks[idx] > 0, to_eval_imgs[idx], 0)
-
-            #canister_diff = canister_gt - canister_real
-            small_bottle_diff = small_bottle_gt - small_bottle_real
-            medium_bottle_diff = medium_bottle_gt - medium_bottle_real
-            large_bottle_diff = large_bottle_gt - large_bottle_real
-            full_diff = blender_depth[idx] - to_eval_imgs[idx]
-
-            ##ACHTUNG CANISTER HAT LOCH --> #PIXEL ANDERS
-            x = np.argwhere(large_bottle_gt[:,:] > 0)[:, 0]
-            y = np.argwhere(large_bottle_gt[:,:] > 0)[:, 1]
-            px_gt = large_bottle_gt[x, y]
-            #print(px_gt[:50])
-                       
-            x = np.argwhere(large_bottle_real[:,:] > 0)[:, 0]
-            y = np.argwhere(large_bottle_real[:,:] > 0)[:, 1]
-            px_real = large_bottle_real[x, y]
-            #print(px_real[:50])
-
-            px_diff = px_gt - px_real
-            
-            #print(px_diff[:50])
-            print("max: ", np.max(px_diff))
-            print("min: ", np.min(px_diff))
-            print("med: ", np.median(px_diff))
-            print("mean: ", np.mean(px_diff))
-            print("std: ", np.std(px_diff))
-            print("var: ", np.var(px_diff))
-
-            #print(np.all(np.equal(px_gt, px_real)))
-                    
-            # plt.imshow(large_bottle_diff, cmap="tab20")
-            # plt.show()
-
-            fig, ax = plt.subplots(nrows=4, ncols=3, figsize=(20, 15))
-            
-            ax[0][0].imshow(canister_gt)
-            ax[0][0].title.set_text("canister_gt")
-            ax[0][1].imshow(canister_real)
-            ax[0][1].title.set_text("canister_real")
-            ax[0][2].imshow(np.random.randint(0,255, size=1280*720).reshape((720,1280)))
-            ax[0][2].title.set_text("canister_diff")
-
-            ax[1][0].imshow(small_bottle_gt)
-            ax[1][0].title.set_text("small_bottle_gt")
-            ax[1][1].imshow(small_bottle_real)
-            ax[1][1].title.set_text("small_bottle_real")
-            ax[1][2].imshow(small_bottle_diff)
-            ax[1][2].title.set_text("small_bottle_diff")
-
-            ax[2][0].imshow(medium_bottle_gt)
-            ax[2][0].title.set_text("medium_bottle_gt")
-            ax[2][1].imshow(medium_bottle_real)
-            ax[2][1].title.set_text("medium_bottle_real")
-            ax[2][2].imshow(medium_bottle_diff)
-            ax[2][2].title.set_text("medium_bottle_diff")
-
-            ax[3][0].imshow(large_bottle_gt)
-            ax[3][0].title.set_text("large_bottle_gt")
-            ax[3][1].imshow(large_bottle_real)
-            ax[3][1].title.set_text("large_bottle_real")
-            ax[3][2].imshow(large_bottle_diff)
-            ax[3][2].title.set_text("large_bottle_diff")
-            # ax = ax.ravel()
-            # divider = make_axes_locatable(ax[0])
-            # cax = divider.append_axes('right', size='5%', pad=0.05)
-            # fig.colorbar(ax[0], cax=cax, orientation='vertical')
-
-            # divider = make_axes_locatable(ax[1])
-            # cax = divider.append_axes('right', size='5%', pad=0.05)
-            # fig.colorbar(ax[1], cax=cax, orientation='vertical')
-
-            plt.tight_layout()
-            plt.show()
+        return [np.max(px_diff), np.min(px_diff), np.median(px_diff), np.mean(px_diff), np.std(px_diff), np.var(px_diff)]
 
 
-            # fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 6))
-            # ax[0][0].imshow(canister_real)
-            # ax[0][0].title.set_text("canister_real")
-            # ax[0][1].imshow(small_bottle_real)
-            # ax[0][1].title.set_text("small_bottle_real")
-            # ax[1][0].imshow(medium_bottle_real)
-            # ax[1][0].title.set_text("medium_bottle_real")
-            # ax[1][1].imshow(large_bottle_real)
-            # ax[1][1].title.set_text("large_bottle_real")
-            # plt.tight_layout()
-            
-            
-            # plt.show()
+def append_to_dict(mydict, filename, scene, vals, object_name):
+    mydict["File"].append(filename)
+    mydict["Scene"].append(scene)
+    mydict["Object"].append(object_name)
+    mydict["Max"].append(vals[0])
+    mydict["Min"].append(vals[1])
+    mydict["Median"].append(vals[2])
+    mydict["Mean"].append(vals[3])
+    mydict["Std"].append(vals[4])
+    mydict["Var"].append(vals[5])
+    return mydict
 
-            break
 
-    
+if __name__ == '__main__':
+    main()
--- misc/training_automation.py
+++ misc/training_automation.py
@@ -3,6 +3,7 @@
 import io
 import signal
 import subprocess
+import shutil
 import numpy as np
 import os
 import time
@@ -10,40 +11,126 @@ import time
 n_samples = np.arange(50, 424, 50, dtype=int)
 n_samples = np.append(n_samples, 424).tolist()
 n_samples = [str(sample) for sample in n_samples]
-print(n_samples)
+
+path_to_master = '/home/julius/Documents/Julius_03_50'
+path_to_scenes = path_to_master + '/scenes'
 
 methods = ["nerfacto", "instant-ngp","instant-ngp-bounded"]
-#methods = ["instant-ngp"]
-sigma = 15
+#methods = ["instant-dex_nerf"]
+sigma = 10
+#iterations = 100000
+
+# scenes = sorted([f.name for f in os.scandir("/home/julius/Julius_03_960x540/scenes") if f.is_dir()])
+# print(scenes)
+
+#scenes = ['001_standing_coated_960x540',  '002_lying_coated_960x540',  '003_standing_liquid_960x540',  '004_lying_liquid_960x540', '005_standing_empty_960x540', '006_lying_empty_960x540']
+scenes = ['001_standing_coated',  '002_lying_coated',  '003_standing_liquid',  '004_lying_liquid', '005_standing_empty', '006_lying_empty']
+#scenes = ['005_standing_empty']
 
 
-scenes = sorted([f.name for f in os.scandir("/home/julius/Julius_03_960x540/scenes") if f.is_dir()])
 
-print(scenes)
+# for scene in scenes:
+#     for method in methods:
+#         if method == "nerfacto":
+#             iterations = 5000
+#         else:
+#             iterations = 10000
 
-#scenes = ["001_standing_coated_960x540", "002_lying_coated_960x540"]
-#scenes = ["002_lying_coated_960x540"]
+#         path1 = '/home/julius/Documents/Julius_03_auswertung/scenes/' + str(scene) + "/" + str(method) + "/depth_nerf_sigma_" + str(sigma) + "_iter_" + str(iterations)#the path where to save resized images
+#         #path2 = '/home/julius/Documents/Julius_03_auswertung/scenes/' + str(scene) + "/" + str(method) + "/depth_norm_nerf_sigma_" + str(sigma) + "_iter_" + str(iterations)#the path where to save resized images
+#         #path3 = '/home/julius/Documents/Julius_03_auswertung/scenes/' + str(scene) + "/" + str(method) + "/rgb_nerf_sigma_" + str(sigma) + "_iter_" + str(iterations) #the path where to save resized images
 
-#cmd_train = "ns-train " + methods[0] + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --data /home/julius/Julius_03_960x540/scenes/" + scenes[0] + " tracebot-data"
-#cmd_eval = "ns-eval --load-config /home/julius/outputs/" + scenes[0] + "/" + methods[0] + "/_____INSERT_FOLDER_HERE______/config.yml" 
-#print(cmd_eval)
+#         # create new folder
+#         if not os.path.exists(path1):
+#             #print(path1)
+#             os.makedirs(path1, exist_ok=False)
+
+#         # if not os.path.exists(path2):
+#         #     #print(path2)
+#         #     os.makedirs(path2, exist_ok=False)
+
+#         # if not os.path.exists(path3):
+#         #     #print(path3)
+#         #     os.makedirs(path3, exist_ok=False)
+
+#         #cmd1 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/depth_nerf " + path1
+#         cmd1 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/depth_instant-dex_nerf " + path1
+#         print(cmd1)
+        
+        #cmd1 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/depth_nerf /home/julius/Documents/" + str(scene) + "/" + str(method) + "/depth_nerf_sigma15"
+        #cmd1 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/005_standing_empty/depth_nerf /home/julius/Documents/Julius_03_auswertung/scenes/005_standing_empty/instant-ngp/depth_nerf_sigma15"
+        # cmd1 = cmd1.split()
+        # cmd2 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/depth_norm_nerf " + path2
+        # cmd2 = cmd2.split()
+        # cmd3 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/rgb_nerf " + path3
+        # cmd3 = cmd3.split()
+
+#         p1 = subprocess.Popen(cmd1, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+#         p1.wait()
+#         p2 = subprocess.Popen(cmd2, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+#         p2.wait()
+#         p3 = subprocess.Popen(cmd3, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+#         p3.wait()
+#         print(cmd1)
+#         print(cmd2)
+#         print(cmd3)
+#         print("")
+#         p1.kill()
+#         p2.kill()
+#         p3.kill()
+
 
 eval_list = []
+cmd_list = []
+master_list = []
 
 for scene in scenes:
     for method in methods:
+        print("Now training method: {} for scene: {}.".format(method, scene))
+
         if method == "nerfacto":
             iterations = 5000
         else:
             iterations = 10000
-        cmd_train = "ns-train " + method + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --max-num-iterations " + str(iterations) + " --data /home/julius/Julius_03_960x540/scenes/" + scene + " tracebot-data"
-        print(cmd_train)
+        path1 = '/home/julius/Documents/Julius_03_auswertung/scenes/' + str(scene) + "/" + str(method) + "/depth_nerf_sigma_" + str(sigma) + "_iter_" + str(iterations)#the path where to save resized images
+        path2 = '/home/julius/Documents/Julius_03_auswertung/scenes/' + str(scene) + "/" + str(method) + "/depth_norm_nerf_sigma_" + str(sigma) + "_iter_" + str(iterations)#the path where to save resized images
+        path3 = '/home/julius/Documents/Julius_03_auswertung/scenes/' + str(scene) + "/" + str(method) + "/rgb_nerf_sigma_" + str(sigma) + "_iter_" + str(iterations) #the path where to save resized images
+
+        # # create new folder
+        # if not os.path.exists(path1):
+        #     #print(path1)
+        #     os.makedirs(path1, exist_ok=False)
+
+        # if not os.path.exists(path2):
+        #     #print(path2)
+        #     os.makedirs(path2, exist_ok=False)
+
+        # if not os.path.exists(path3):
+        #     #print(path3)
+        #     os.makedirs(path3, exist_ok=False)
+
+        cmd1 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/depth_nerf " + path1
+        #cmd1 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/depth_nerf /home/julius/Documents/" + str(scene) + "/" + str(method) + "/depth_nerf_sigma15"
+        #cmd1 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/005_standing_empty/depth_nerf /home/julius/Documents/Julius_03_auswertung/scenes/005_standing_empty/instant-ngp/depth_nerf_sigma15"
+        #cmd1 = cmd1.split()
+        cmd2 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/depth_norm_nerf " + path2
+        #cmd2 = cmd2.split()
+        cmd3 = "scp -r julius@192.168.141.40:/home/julius/Julius_03/scenes/" + str(scene) + "/rgb_nerf " + path3
+        #cmd3 = cmd3.split()
+        cmd_list.append(cmd1)
+        cmd_list.append(cmd2)
+        cmd_list.append(cmd3)
+
+        #cmd_train = "ns-train " + method + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --max-num-iterations " + str(iterations) + " --data /home/julius/005_standing_empty" +" tracebot-data"
+        cmd_train = "ns-train " + method + " --pipeline.model.depth-render-method dex-nerf --pipeline.model.sigma_thresh " + str(sigma) + " --max-num-iterations " + str(iterations) + " --data /home/julius/Julius_03/scenes/" + scene + " tracebot-data"
+        #print(cmd_train)
         #cmd_eval = "ns-eval --load-config /home/julius/outputs/" + scene + "/" + method 
-        cmd_train = cmd_train.split()
+        
 
         try:
             #subprocess.check_call(cmd_train, cwd=os.getcwd())
             print("Trying to run: ", cmd_train)
+            cmd_train = cmd_train.split()
             p = subprocess.Popen(cmd_train, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             flag = False
             #print("CHECK OUTPUT: ", subprocess.check_output(cmd_train, stderr=subprocess.STDOUT))
@@ -63,28 +150,43 @@ for scene in scenes:
                 
                 if "Saving config to:" in line:
                     flag = True
+                    # print(line)
+
+                    # start_index = line.find("outputs")
+                    # end_index = line.find("experiment")-1
+
+                    # extracted_file_path = line[start_index:end_index]
+                    # cmd_eval = "ns-eval --load-config /home/julius/" + extracted_file_path #str(line.replace(" ", ""))
+                    # cmd_eval = cmd_eval.replace("\n", "")
+                    # print(cmd_eval)
+                    # #cmd_eval = cmd_eval.split()
+                    # eval_list.append(cmd_eval)
                    
                 if "Training Finished" in line:
                     time.sleep(2)
                     p.send_signal(signal.SIGINT)
             
             p.wait()
-            # print("")
-            # print("----------------------------------SUCCESS")
-            # print("")
-            # p.send_signal(signal.SIGINT)
+
         except subprocess.CalledProcessError:
             print("There was an error with command ____cmd_train____ exited with non-zero code!")
 
-        print("")
-        print("Now training next scene.")
+    
+        print("Finished method: {} for scene: {}.".format(method, scene))
         print("")
         p.kill()
+
 print("")
 print("")
 print("")
 print("----------Finished training----------")
+
 print("Eval List: ", eval_list)
+print("")
+print("Cmd List: ", cmd_list)
+print("")
+master_list = list(zip(eval_list, cmd_list))
+print("Master List: ", master_list)
 
 # p1 =  subprocess.Popen(eval_list[0], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 # p2 =  subprocess.Popen(eval_list[1], stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
